3d3a50e3df57ff52d8ac73cac8b35797c5240999
Steve Berneel <steve@berneel.be>
2014-05-24 00:20:45 +0200

    update Helium.provision

--- helium-provision.sh
+++ helium-provision.sh
@@ -271,13 +271,13 @@ cat > /etc/named.conf << EOF
 //
 
 options {
-        listen-on port 53 { 127.0.0.1; 192.168.64.2; };
+        listen-on port 53 { any; };
         listen-on-v6 port 53 { ::1; };
         directory "/var/named"; 
         dump-file "/var/named/data/cache_dump.db"; 
         statistics-file "/var/named/data/named_stats.txt"; 
         memstatistics-file "/var/named/data/named_mem_stats.txt"; 
-        allow-query     { any; };
+        allow-query     { localnets; };
         allow-transfer  { localhost; 192.168.64.3; };
         recursion yes; 
         dnssec-enable yes; 
